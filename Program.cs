c62e2ad4e2b47cc38971144e8827cd29e85a7df2
kedar <dhagekedarnath70@gamil.com>
2024-04-16 00:43:54 +0530

    Changes in program class

--- Program.cs
+++ Program.cs
@@ -12,6 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<EmployeeContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("CRUDREACT")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(opt =>
 {
