380d81aa5f4071e74cc9c2afaf2089289df8e583
kedar <dhagekedarnath70@gamil.com>
2024-04-16 00:12:32 +0530

    first commit

---
+++ Controllers/AuthonticationController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.IdentityModel.Tokens;
+using ReactCrudAPI.CommonFunctions;
+using ReactCrudAPI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ReactCrudAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowOrigin")]
+    public class AuthonticationController : ControllerBase
+    {
+
+        private readonly EmployeeContext _employeeContext;
+        private readonly IConfiguration _configuration;
+        public AuthonticationController(EmployeeContext employeeContext, IConfiguration configuration)
+        {
+            _employeeContext = employeeContext;
+            _configuration = configuration;
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> PostLoginDetails(UserDetail userModel)
+        {
+            try
+            {
+                if (userModel != null)
+                {
+                    var result = _employeeContext.userModels.Where(t => t.Email == userModel.Email && t.Password == userModel.Password).FirstOrDefault();
+                    if (string.IsNullOrEmpty(result.Email))
+                    {
+                        return BadRequest("Invalide Credntials");
+                    }
+                    else
+                    {
+                        userModel.UserMessage = "Login Success";
+                        userModel.AccessToken = GenerateToken.GetToken(userModel, _configuration);
+                        return Ok(userModel);
+                    }
+                }
+                else
+                {
+                    return BadRequest("No data found");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using ReactCrudAPI.Models;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<EmployeeContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("CRUDREACT")));
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPI", Version = "v1" });
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+builder.Services.AddCors(c =>
+{
+    c.AddPolicy("AllowOrigin",
+            builder => builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        ValidateIssuerSigningKey = true
+
+
+    };
+});
+
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseCors("AllowOrigin");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+
+//app.MapControllers();
+
+app.Run();
+
